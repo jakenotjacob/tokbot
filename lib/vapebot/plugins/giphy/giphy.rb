94f141380cf5c687669eab424444eb78af929431
Jake Campbell <jake.campbell91@gmail.com>
2016-04-27 23:02:37 -0600

    Begin Giphy plugin

---
+++ lib/vapebot/plugins/giphy/giphy.rb
@@ -0,0 +1,45 @@
+class Giphy < Vapebot::Plugin
+  PUBLIC_API_KEY = "dc6zaTOxFJmzC"
+  URL = "http://api.giphy.com/v1/gifs/"
+  attr_reader :link
+  def initialize(tags)
+    @tags = tags
+  end
+
+  def self.new(args=nil)
+    if args
+      super
+    else
+      "Usage: '!giphy a b c'"
+    end
+  end
+
+  def execute
+    random
+  end
+
+  def response
+    Net::HTTP.get URI(URL+api_str+"&"+tag_list)
+  end
+
+  def random
+    JSON.parse(response)["data"]["url"]
+  end
+
+  #---
+
+  def api_str
+    "api_key=#{PUBLIC_API_KEY}"
+  end
+
+  def tag_list
+    "tag=#{@tags.join("+")}"
+  end
+
+  #Parse json response
+  def parse_json
+    JSON.parse(get_json)
+  end
+
+end
+
